ad3b1aad6450ca1b5245f0e90dd6eaeeb3bff919
Sebastian Scurtescu <seb@pop-os.localdomain>
2020-11-04 11:50:45 -0800

    Attempting to try tasks to set controls onto a different task

--- AAAAaaaaaahhhhhhhhh/src/main.cpp
+++ AAAAaaaaaahhhhhhhhh/src/main.cpp
@@ -205,44 +205,7 @@ void buttonPressed() {
 
 }
 
-void usercontrol(void) {
-
-  // Define bob to keep track of values for the robot
-  // Bob definition start----------------------------------------------------
-  robot bob;
-
-  // Define current acceleration and rotation for the robot
-  bob.x_accl = 0;
-  bob.y_accl = 0;
-  bob.z_accl = 0;
-  bob.x_rot = 0;
-  bob.y_rot = 0;
-  bob.z_rot = 0;
-
-  // Setting motor values for the robot
-  bob.D1_value = 0;
-  bob.D2_value = 0;
-  bob.D3_value = 0;
-  bob.D4_value = 0;
-
-  // Initializing brake counters
-  bob.D1D3Brake_counter = 0;
-  bob.D2D4Brake_counter = 0;
-
-  // Initilize stationary counter
-  bob.stationary = true;
-  // Bob definition end-------------------------------------------------------
-
-  // User control code here, inside the loop
-  while (1) {
-
-    // Deal with button presses for special functions for the controller
-    if (Controller1.ButtonA.pressed()) {
-
-    	bob.zeroInertial();
-    	Controller1.Screen.setCursor(1, 1);
-
-    }
+void controls(robot bob) {
 
     // Set motor values for forward and backward motion
     bob.D1_value = Controller1.Axis3.position();
@@ -318,6 +281,126 @@ void usercontrol(void) {
 
     }
     // Brake functions end--------------------------------------------------------
+
+}
+
+void usercontrol(void) {
+
+  // Define bob to keep track of values for the robot
+  // Bob definition start----------------------------------------------------
+  robot bob;
+
+  // Define current acceleration and rotation for the robot
+  bob.x_accl = 0;
+  bob.y_accl = 0;
+  bob.z_accl = 0;
+  bob.x_rot = 0;
+  bob.y_rot = 0;
+  bob.z_rot = 0;
+
+  // Setting motor values for the robot
+  bob.D1_value = 0;
+  bob.D2_value = 0;
+  bob.D3_value = 0;
+  bob.D4_value = 0;
+
+  // Initializing brake counters
+  bob.D1D3Brake_counter = 0;
+  bob.D2D4Brake_counter = 0;
+
+  // Initilize stationary counter
+  bob.stationary = true;
+  // Bob definition end-------------------------------------------------------
+
+  // Start Tasks----------------------------------------------------------------
+  vex::Task controls (controls(bob));
+  // End Tasks------------------------------------------------------------------
+
+  // User control code here, inside the loop
+  while (1) {
+
+    // Deal with button presses for special functions for the controller
+    if (Controller1.ButtonA.pressed()) {
+
+    	bob.zeroInertial();
+    	Controller1.Screen.setCursor(1, 1);
+
+    }
+
+    // // Set motor values for forward and backward motion
+    // bob.D1_value = Controller1.Axis3.position();
+    // bob.D2_value = -Controller1.Axis3.position();
+    // bob.D3_value = -Controller1.Axis4.position();
+    // bob.D4_value = Controller1.Axis4.position();
+    //
+    // // Add turning into motor values
+    // bob.D1_value = bob.D1_value + Controller1.Axis1.position();
+    // bob.D2_value = bob.D2_value + Controller1.Axis1.position();
+    // bob.D3_value = bob.D3_value + Controller1.Axis1.position();
+    // bob.D4_value = bob.D4_value + Controller1.Axis1.position();
+    //
+    // // Set tower and gripper motor values
+    // bob.Tower_value = -Controller2.Axis1.position();
+    // bob.RGrip_value = Controller2.Axis2.position();
+    // bob.LGrip_value = Controller2.Axis4.position();
+    //
+    // if (Controller1.ButtonB.pressed()) {
+    //
+    //     bob.RGrip_value = 100;
+    //
+    // };
+    //
+    // // Move wheels and tower motors
+    // bob.D1.spin(vex::directionType::fwd, bob.D1_value, vex::velocityUnits::pct);
+    // bob.D2.spin(vex::directionType::fwd, bob.D2_value, vex::velocityUnits::pct);
+    // bob.D3.spin(vex::directionType::fwd, bob.D3_value, vex::velocityUnits::pct);
+    // bob.D4.spin(vex::directionType::fwd, bob.D4_value, vex::velocityUnits::pct);
+    //
+    // bob.T1.spin(vex::directionType::fwd, bob.Tower_value, vex::velocityUnits::pct);
+    // bob.I1.spin(vex::directionType::fwd, bob.RGrip_value, vex::velocityUnits::pct);
+    // bob.I2.spin(vex::directionType::fwd, bob.LGrip_value, vex::velocityUnits::pct);
+    //
+    // // Brakes to apply motor brakes for a short period of time after the user stops moving the robot
+    // // to avoid drifting
+    // // Brake start---------------------------------------------------------------
+    // if (Controller1.Axis3.position() != 0) {
+    //
+    //   bob.D1D3Brake_counter = 0;
+    //
+    // }
+    //
+    // if (Controller1.Axis3.position() == 0) {
+    //
+    //   while (bob.D1D3Brake_counter <= 30) {
+    //
+    //     bob.D1.setBrake(brakeType::brake);
+    //     bob.D3.setBrake(brakeType::brake);
+    //
+    //     bob.D1D3Brake_counter++;
+    //
+    //   }
+    //
+    // }
+    //
+    // if (Controller1.Axis4.position() != 0) {
+    //
+    //   bob.D2D4Brake_counter = 0;
+    //
+    // }
+    //
+    // if (Controller1.Axis4.position() == 0) {
+    //
+    //   while (bob.D2D4Brake_counter <= 30) {
+    //
+    //     bob.D2.setBrake(brakeType::brake);
+    //     bob.D4.setBrake(brakeType::brake);
+    //
+    //     bob.D2D4Brake_counter++;
+    //
+    //   }
+    //
+    // }
+    // // Brake functions end--------------------------------------------------------
 
     // Update functions start-----------------------------------------------------
     // bob.updateInertial();
