2b0ef097e8689317a3ed7dfbb6de277b72a67f54
Sebastian Scurtescu <seb@pop-os.localdomain>
2020-11-04 11:53:53 -0800

    Fixed issue for controller task not looping and just exiting

--- AAAAaaaaaahhhhhhhhh/src/main.cpp
+++ AAAAaaaaaahhhhhhhhh/src/main.cpp
@@ -207,81 +207,83 @@ void buttonPressed() {
 
 void controls(robot bob) {
 
-    // Set motor values for forward and backward motion
-    bob.D1_value = Controller1.Axis3.position();
-    bob.D2_value = -Controller1.Axis3.position();
-    bob.D3_value = -Controller1.Axis4.position();
-    bob.D4_value = Controller1.Axis4.position();
+    while (1) {
 
-    // Add turning into motor values
-    bob.D1_value = bob.D1_value + Controller1.Axis1.position();
-    bob.D2_value = bob.D2_value + Controller1.Axis1.position();
-    bob.D3_value = bob.D3_value + Controller1.Axis1.position();
-    bob.D4_value = bob.D4_value + Controller1.Axis1.position();
+        // Set motor values for forward and backward motion
+        bob.D1_value = Controller1.Axis3.position();
+        bob.D2_value = -Controller1.Axis3.position();
+        bob.D3_value = -Controller1.Axis4.position();
+        bob.D4_value = Controller1.Axis4.position();
 
-    // Set tower and gripper motor values
-    bob.Tower_value = -Controller2.Axis1.position();
-    bob.RGrip_value = Controller2.Axis2.position();
-    bob.LGrip_value = Controller2.Axis4.position();
+        // Add turning into motor values
+        bob.D1_value = bob.D1_value + Controller1.Axis1.position();
+        bob.D2_value = bob.D2_value + Controller1.Axis1.position();
+        bob.D3_value = bob.D3_value + Controller1.Axis1.position();
+        bob.D4_value = bob.D4_value + Controller1.Axis1.position();
 
-    if (Controller1.ButtonB.pressed()) {
+        // Set tower and gripper motor values
+        bob.Tower_value = -Controller2.Axis1.position();
+        bob.RGrip_value = Controller2.Axis2.position();
+        bob.LGrip_value = Controller2.Axis4.position();
 
-        bob.RGrip_value = 100;
+        if (Controller1.ButtonB.pressed()) {
 
-    };
+            bob.RGrip_value = 100;
 
-    // Move wheels and tower motors
-    bob.D1.spin(vex::directionType::fwd, bob.D1_value, vex::velocityUnits::pct);
-    bob.D2.spin(vex::directionType::fwd, bob.D2_value, vex::velocityUnits::pct);
-    bob.D3.spin(vex::directionType::fwd, bob.D3_value, vex::velocityUnits::pct);
-    bob.D4.spin(vex::directionType::fwd, bob.D4_value, vex::velocityUnits::pct);
+        };
 
-    bob.T1.spin(vex::directionType::fwd, bob.Tower_value, vex::velocityUnits::pct);
-    bob.I1.spin(vex::directionType::fwd, bob.RGrip_value, vex::velocityUnits::pct);
-    bob.I2.spin(vex::directionType::fwd, bob.LGrip_value, vex::velocityUnits::pct);
+        // Move wheels and tower motors
+        bob.D1.spin(vex::directionType::fwd, bob.D1_value, vex::velocityUnits::pct);
+        bob.D2.spin(vex::directionType::fwd, bob.D2_value, vex::velocityUnits::pct);
+        bob.D3.spin(vex::directionType::fwd, bob.D3_value, vex::velocityUnits::pct);
+        bob.D4.spin(vex::directionType::fwd, bob.D4_value, vex::velocityUnits::pct);
 
-    // Brakes to apply motor brakes for a short period of time after the user stops moving the robot
-    // to avoid drifting
-    // Brake start---------------------------------------------------------------
-    if (Controller1.Axis3.position() != 0) {
+        bob.T1.spin(vex::directionType::fwd, bob.Tower_value, vex::velocityUnits::pct);
+        bob.I1.spin(vex::directionType::fwd, bob.RGrip_value, vex::velocityUnits::pct);
+        bob.I2.spin(vex::directionType::fwd, bob.LGrip_value, vex::velocityUnits::pct);
 
-      bob.D1D3Brake_counter = 0;
+        // Brakes to apply motor brakes for a short period of time after the user stops moving the robot
+        // to avoid drifting
+        // Brake start---------------------------------------------------------------
+        if (Controller1.Axis3.position() != 0) {
 
+          bob.D1D3Brake_counter = 0;
+
+        }
+
+        if (Controller1.Axis3.position() == 0) {
+
+          while (bob.D1D3Brake_counter <= 30) {
+
+            bob.D1.setBrake(brakeType::brake);
+            bob.D3.setBrake(brakeType::brake);
+
+            bob.D1D3Brake_counter++;
+
+          }
+
+        }
+
+        if (Controller1.Axis4.position() != 0) {
+
+          bob.D2D4Brake_counter = 0;
+
+        }
+
+        if (Controller1.Axis4.position() == 0) {
+
+          while (bob.D2D4Brake_counter <= 30) {
+
+            bob.D2.setBrake(brakeType::brake);
+            bob.D4.setBrake(brakeType::brake);
+
+            bob.D2D4Brake_counter++;
+
+          }
+
+        }
+        // Brake functions end--------------------------------------------------------
     }
-
-    if (Controller1.Axis3.position() == 0) {
-
-      while (bob.D1D3Brake_counter <= 30) {
-
-        bob.D1.setBrake(brakeType::brake);
-        bob.D3.setBrake(brakeType::brake);
-
-        bob.D1D3Brake_counter++;
-
-      }
-
-    }
-
-    if (Controller1.Axis4.position() != 0) {
-
-      bob.D2D4Brake_counter = 0;
-
-    }
-
-    if (Controller1.Axis4.position() == 0) {
-
-      while (bob.D2D4Brake_counter <= 30) {
-
-        bob.D2.setBrake(brakeType::brake);
-        bob.D4.setBrake(brakeType::brake);
-
-        bob.D2D4Brake_counter++;
-
-      }
-
-    }
-    // Brake functions end--------------------------------------------------------
-
 }
 
 void usercontrol(void) {
