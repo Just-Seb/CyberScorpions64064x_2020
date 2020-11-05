9f8d6b935368081146e7d238fba2625716d19ffc
Sebastian Scurtescu <sebastian.scurtescu@gmail.com>
2020-11-05 14:18:48 -0800

    moved robot class to separate file and added header file

---
+++ AAAAaaaaaahhhhhhhhh/include/classes.h
@@ -0,0 +1,58 @@
+#include "vex.h"
+
+class robot {
+
+  public:
+
+  motor D1;
+  motor D2;
+  motor D3;
+  motor D4;
+
+  motor T1;
+
+  motor I1;
+  motor I2;
+
+  float x_accl;
+  float y_accl;
+  float z_accl;
+  float x_rot;
+  float y_rot;
+  float z_rot;
+
+  float x_vel;
+  float y_vel;
+  float z_vel;
+
+  float x_vel_n;
+  float y_vel_n;
+  float z_vel_n;
+
+  float y_offset;
+  float x_offset;
+
+  float x_distance;
+  float y_distance;
+  float z_distance;
+
+  int D1_value;
+  int D2_value;
+  int D3_value;
+  int D4_value;
+
+  int Tower_value;
+  int RGrip_value;
+  int LGrip_value;
+
+  int D1D3Brake_counter;
+  int D2D4Brake_counter;
+
+  float time_last;
+
+  robot();
+  void updateInertial();
+  void calculateVelocity();
+  void zeroInertial();
+
+};
