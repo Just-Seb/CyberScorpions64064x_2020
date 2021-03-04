5a29e8a2797af42c21c0f5826c780b386a630689
Just-Seb <sebastian.scurtescu@gmail.com>
2021-03-03 17:52:46 -0800

    Update main.cpp

--- FuckItDoesn'tWork/src/main.cpp
+++ FuckItDoesn'tWork/src/main.cpp
@@ -13,13 +13,17 @@
 // D1                   motor         18              
 // D2                   motor         2               
 // D3                   motor         10              
-// D4                   motor         19              
-// In1                  motor         20              
-// T1                   motor         17              
-// L1                   motor         3               
-// L2                   motor         8               
+// D4                   motor         20              
+// G1                   motor         16              
+// G2                   motor         17              
 // Controller1          controller                    
 // Controller2          controller                    
+// ultraSensorForward   sonar         A, B            
+// ultraSensorRight     sonar         C, D            
+// Launcher2            motor         12              
+// Launcher             motor         9               
+// Vision15             vision        15              
+// Inertial5            inertial      5               
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
@@ -30,119 +34,203 @@ using namespace vex;
 competition Competition;
 
 // define your global instances of motors and other devices here
+int D1_value;
+int D2_value;
+int D3_value;
+int D4_value;
+int D1D3Brake_counter;
+int D2D4Brake_counter;
+int Launch_Value;
+float xValue;
+float yValue;
 
-/*---------------------------------------------------------------------------*/
-/*                          Pre-Autonomous Functions                         */
-/*                                                                           */
-/*  You may want to perform some actions before the competition starts.      */
-/*  Do them in the following function.  You must return from this function   */
-/*  or the autonomous and usercontrol tasks will not be started.  This       */
-/*  function is only called once after the V5 has been powered on and        */
-/*  not every time that the robot is disabled.                               */
-/*---------------------------------------------------------------------------*/
+int moves[11];
+
 
 void pre_auton(void) {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
 
+
+
   // All activities that occur before the competition starts
   // Example: clearing encoders, setting servo positions, ...
 }
 
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              Autonomous Task                              */
-/*                                                                           */
-/*  This task is used to control your robot during the autonomous phase of   */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
+// Locking.spin(vex::directionType::fwd, -100, vex::velocityUnits::pct);
+//   wait(500, msec);
+//   Locking.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+
+//   D2.spin(vex::directionType::rev, 100, vex::velocityUnits::pct);
+//   D4.spin(vex::directionType::fwd, 100, vex::velocityUnits::pct);
+//   wait(500, msec);
+//   D2.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+//   D4.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+
+//   D1.spin(vex::directionType::rev, -100, vex::velocityUnits::pct);
+//   D3.spin(vex::directionType::fwd, -100, vex::velocityUnits::pct);
+//   wait(560, msec);
+//   D1.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+//   D3.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+
+//   D2.spin(vex::directionType::rev, 50, vex::velocityUnits::pct);
+//   D4.spin(vex::directionType::fwd, 50, vex::velocityUnits::pct);
+//   wait(1700, msec);
+//   D2.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+//   D4.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+
+
+
+//   D1.spin(vex::directionType::fwd, 100, vex::velocityUnits::pct);
+//   D3.spin(vex::directionType::fwd, 100, vex::velocityUnits::pct);
+//   D2.spin(vex::directionType::fwd, 100, vex::velocityUnits::pct);
+//   D4.spin(vex::directionType::fwd, 100, vex::velocityUnits::pct);
+//   wait(500, msec);
+//   D1.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+//   D3.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+//   D2.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+//   D4.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+
+//   D1.spin(vex::directionType::fwd, -100, vex::velocityUnits::pct);
+//   D3.spin(vex::directionType::fwd, -100, vex::velocityUnits::pct);
+//   D2.spin(vex::directionType::fwd, -100, vex::velocityUnits::pct);
+//   D4.spin(vex::directionType::fwd, -100, vex::velocityUnits::pct);
+//   wait(500, msec);
+//   D1.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+//   D3.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+//   D2.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+//   D4.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+
+//   D1.spin(vex::directionType::fwd, 100, vex::velocityUnits::pct);
+//   D3.spin(vex::directionType::fwd, 100, vex::velocityUnits::pct);
+//   D2.spin(vex::directionType::fwd, 100, vex::velocityUnits::pct);
+//   D4.spin(vex::directionType::fwd, 100, vex::velocityUnits::pct);
+//   wait(500, msec);
+//   D1.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+//   D3.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+//   D2.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+//   D4.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+
+//   D1.spin(vex::directionType::fwd, -100, vex::velocityUnits::pct);
+//   D3.spin(vex::directionType::fwd, -100, vex::velocityUnits::pct);
+//   D2.spin(vex::directionType::fwd, -100, vex::velocityUnits::pct);
+//   D4.spin(vex::directionType::fwd, -100, vex::velocityUnits::pct);
+//   wait(500, msec);
+//   D1.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+//   D3.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+//   D2.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+//   D4.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+
+//   D1.spin(vex::directionType::fwd, -100, vex::velocityUnits::pct);
+//   D3.spin(vex::directionType::fwd, -100, vex::velocityUnits::pct);
+//   D2.spin(vex::directionType::fwd, -100, vex::velocityUnits::pct);
+//   D4.spin(vex::directionType::fwd, -100, vex::velocityUnits::pct);
+//   wait(500, msec);
+//   D1.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+//   D3.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+//   D2.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+//   D4.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+
+//   D1.spin(vex::directionType::fwd, 100, vex::velocityUnits::pct);
+//   D3.spin(vex::directionType::fwd, 100, vex::velocityUnits::pct);
+//   D2.spin(vex::directionType::fwd, 100, vex::velocityUnits::pct);
+//   D4.spin(vex::directionType::fwd, 100, vex::velocityUnits::pct);
+//   wait(500, msec);
+//   D1.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+//   D3.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+//   D2.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+//   D4.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+
 
 void autonomous(void) {
-  // ..........................................................................
-  // Insert autonomous user code here.
-  // ..........................................................................
+  
 }
 
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              User Control Task                            */
-/*                                                                           */
-/*  This task is used to control your robot during the user control phase of */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
+void adjust() {
+  xValue = xValue * cos()
+}
 
-int D1_value;
-int D2_value;
-int D3_value;
-int D4_value;
-int T1_value;
-int D1D3Brake_counter;
-int D2D4Brake_counter;
-int L1_value;
-int L2_value;
-int In1_value;
+void getControllerValues(void) {
+  yValue = Controller1.Axis3.position();
+  xValue = Controller1.Axis4.position();
+}
+
+void DriveThingiFunction(void) {
+  getControllerValues();
+
+  adjust();
+
+  int turning = Controller1.Axis1.position();
+
+  D1.spin(vex::directionType::fwd, (xValue + turning), vex::velocityUnits::pct);
+  D2.spin(vex::directionType::fwd, (-yValue + turning), vex::velocityUnits::pct);
+  D3.spin(vex::directionType::fwd, (-xValue + turning), vex::velocityUnits::pct);
+  D4.spin(vex::directionType::fwd, (yValue + turning), vex::velocityUnits::pct);
+
+
+}
 
 void usercontrol(void) {
   // User control code here, inside the loop
   while (1) {
-    // Set motor values for forward and backward motion
-    D1_value = Controller1.Axis1.position();
-    D3_value = -Controller1.Axis1.position();
-    D2_value = -Controller1.Axis2.position();
-    D4_value = Controller1.Axis2.position();
+    // // Set motor values for forward and backward motion
+    // D1_value = Controller1.Axis4.position();
+    // D3_value = -Controller1.Axis4.position();
+    // D2_value = -Controller1.Axis3.position();
+    // D4_value = Controller1.Axis3.position();
 
-    // Add turning into motor values
-    D1_value = D1_value + Controller1.Axis4.position();
-    D2_value = D2_value + Controller1.Axis4.position();
-    D3_value = D3_value + Controller1.Axis4.position();
-    D4_value = D4_value + Controller1.Axis4.position();
+    // // Add turning into motor values
+    // D1_value = D1_value + Controller1.Axis1.position();
+    // D2_value = D2_value + Controller1.Axis1.position();
+    // D3_value = D3_value + Controller1.Axis1.position();
+    // D4_value = D4_value + Controller1.Axis1.position();
 
-    // Set tower and gripper motor values
-    // T1_value = -Controller2.Axis3.position();
-    // bob.RGrip_value = Controller2.Axis2.position();
-    // bob.LGrip_value = Controller2.Axis4.position();
 
-    L1_value = Controller1.Axis3.position();
-    L2_value = -Controller1.Axis3.position();
+    // // Move wheels and tower motors
+    // D1.spin(vex::directionType::fwd, D1_value, vex::velocityUnits::pct);
+    // D2.spin(vex::directionType::fwd, D2_value, vex::velocityUnits::pct);
+    // D3.spin(vex::directionType::fwd, D3_value, vex::velocityUnits::pct);
+    // D4.spin(vex::directionType::fwd, D4_value, vex::velocityUnits::pct);
+
+    DriveThingiFunction();
+
+    Controller1.Screen.setCursor(3, 3);
+    Controller1.Screen.print(Inertial5.angle());
 
     if (Controller1.ButtonL1.pressing()) {
-      T1_value = 100;
-    } else if (Controller1.ButtonL2.pressing()) {
-      T1_value = -100;
-    } else {
-      T1_value = 0;
+      G1.spin(vex::directionType::fwd, -100, vex::velocityUnits::pct);
+      G2.spin(vex::directionType::fwd, -100, vex::velocityUnits::pct);
     }
+
+    if (Controller1.ButtonL2.pressing()) {
+      G1.spin(vex::directionType::fwd, 100, vex::velocityUnits::pct);
+      G2.spin(vex::directionType::fwd, 100, vex::velocityUnits::pct);
+    }
+
+    if (!Controller1.ButtonL1.pressing() && !Controller1.ButtonL2.pressing()) {
+      G1.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+      G2.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+    }
+
+
 
     if (Controller1.ButtonR1.pressing()) {
-      In1_value = 100;
-    } else if (Controller1.ButtonR2.pressing()) {
-      In1_value = -100;
-    } else {
-      In1_value = 0;
+      Launcher.spin(vex::directionType::fwd, -90, vex::velocityUnits::pct);
+      Launcher2.spin(vex::directionType::fwd, 100, vex::velocityUnits::pct);
+      Controller1.Screen.setCursor(1, 1);
+      Controller1.Screen.print("somthing");
     }
 
-    // Move wheels and tower motors
-    D1.spin(vex::directionType::fwd, D1_value, vex::velocityUnits::pct);
-    D2.spin(vex::directionType::fwd, D2_value, vex::velocityUnits::pct);
-    D3.spin(vex::directionType::fwd, D3_value, vex::velocityUnits::pct);
-    D4.spin(vex::directionType::fwd, D4_value, vex::velocityUnits::pct);
+    if (Controller1.ButtonR2.pressing()) {
+      Launcher.spin(vex::directionType::fwd, 90, vex::velocityUnits::pct);
+      Launcher2.spin(vex::directionType::fwd, -100, vex::velocityUnits::pct);
+    }
+    if (!Controller1.ButtonR1.pressing() && !Controller1.ButtonR2.pressing()) {
+      Launcher.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+      Launcher2.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);
+    }
 
-    T1.spin(vex::directionType::fwd, T1_value, vex::velocityUnits::pct);
-
-    L1.spin(vex::directionType::fwd, L1_value, vex::velocityUnits::pct);
-    L2.spin(vex::directionType::fwd, L2_value, vex::velocityUnits::pct);
-    Controller1.Screen.setCursor(3, 3);
-    Controller1.Screen.print(L1_value);
-    Controller1.Screen.setCursor(3, 7);
-    Controller1.Screen.setCursor(1, 2);
-    Controller1.Screen.print(L2_value);
-
-    In1.spin(vex::directionType::fwd, In1_value, vex::velocityUnits::pct);
-
+    
     // Brakes to apply motor brakes for a short period of time after the user stops moving the robot
     // to avoid drifting
     // Brake start---------------------------------------------------------------
